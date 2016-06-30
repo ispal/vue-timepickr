649fd15af5a88abcb2d89c80646e3a256982361c
Irko Palenius <ispal84@gmail.com>
2016-06-30 21:48:38 +0300

    Initial elements

---
+++ src/Timepicker.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="timepicker">
+    <div class="timepicker__header">
+      Set time
+    </div>
+    <div class="timepicker__time">
+       <div class="timepicker__unit">0</div>
+       <div class="timepicker__unit">0</div>
+       <div class="timepicker__separator">:</div>
+       <div class="timepicker__unit">0</div>
+       <div class="timepicker__unit">0</div>
+    </div>
+    <div class="timepicker__digits">
+      <div class="timepicker__digit" v-for="digit in digits">
+        {{ digit }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      digits: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+$header-bg: #07BEB8;
+$time-bg: #3DCCC7;
+
+.timepicker {
+  background: #fff;
+  width: 200px;
+  box-shadow: 0 3px 10px rgba(0,0,0,.3);
+
+  &__header {
+    padding: 5px 15px;
+    font-size: 14px;
+    color: #fff;
+    background: $header-bg;
+  }
+  &__time {
+    background: $time-bg;
+    display: flex;
+    justify-content: center;
+    font-size: 40px;
+    line-height: 1;
+    padding: 10px;
+  }
+
+  &__digits {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 5px 20px;
+  }
+
+  &__digit {
+    width: 33%;
+    padding: 15px 0;
+    text-align: center;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import Timepicker from './Timepicker';
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
-  components: { App }
-})
+  components: { Timepicker }
+});
